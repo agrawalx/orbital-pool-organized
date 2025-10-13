6b2b27fdb1d16a69a79acb3901efb8b6f6a53a7a
Ishaan Kesarwani <kesarwaniishaan4@gmail.com>
2025-10-13 11:29:26 +0530

    added tests to the functions

--- contracts/stylus/src/lib.rs
+++ contracts/stylus/src/lib.rs
@@ -1,21 +1,16 @@
-#![cfg_attr(not(feature = "export-abi"), no_main)]
-#![allow(non_snake_case)] // Allows camelCase function names to match Solidity.
-
-#[macro_use]
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+#![allow(non_snake_case)]
 extern crate alloc;
 
-mod orbital_helper;
-
-use alloc::vec::Vec;
 use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageU256};
 use alloy_primitives::aliases::U144;
+use alloc::vec::Vec;
 
+pub mod orbital_helper;
 use orbital_helper::fixed_point::*;
 use orbital_helper::parameters::*;
 use orbital_helper::invariant;
 
-// Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
 #[storage]
 #[entrypoint]
 pub struct OrbitalHelper {
@@ -26,92 +21,96 @@ pub struct OrbitalHelper {
 #[public]
 impl OrbitalHelper {
     // Forward calls to the modularized functions
-    pub fn convert_to_Q96X48(value: U144) -> U144 {
-        convert_to_Q96X48(value)
+    pub fn convert_to_Q96X48(&self, value: U144) -> Result<U144, Vec<u8>> {
+        Ok(convert_to_Q96X48(value))
     }
 
-    pub fn convert_from_Q96X48(value: U144) -> U144 {
-        convert_from_Q96X48(value)
+    pub fn convert_from_Q96X48(&self, value: U144) -> Result<U144, Vec<u8>> {
+        Ok(convert_from_Q96X48(value))
     }
 
-    pub fn add_Q96X48(a: U144, b: U144) -> U144 {
-        add_Q96X48(a, b)
+    pub fn add_Q96X48(&self, a: U144, b: U144) -> Result<U144, Vec<u8>> {
+        Ok(add_Q96X48(a, b))
     }
 
-    pub fn sub_Q96X48(a: U144, b: U144) -> U144 {
-        sub_Q96X48(a, b)
+    pub fn sub_Q96X48(&self, a: U144, b: U144) -> Result<U144, Vec<u8>> {
+        Ok(sub_Q96X48(a, b))
     }
 
-    pub fn mul_Q96X48(a: U144, b: U144) -> U144 {
-        mul_Q96X48(a, b)
+    pub fn mul_Q96X48(&self, a: U144, b: U144) -> Result<U144, Vec<u8>> {
+        Ok(mul_Q96X48(a, b))
     }
 
-    pub fn div_Q96X48(a: U144, b: U144) -> U144 {
-        div_Q96X48(a, b)
+    pub fn div_Q96X48(&self, a: U144, b: U144) -> Result<U144, Vec<u8>> {
+        Ok(div_Q96X48(a, b))
     }
 
-    pub fn sqrt_Q96X48(y: U144) -> U144 {
-        sqrt_Q96X48(y)
+    pub fn sqrt_Q96X48(&self, y: U144) -> Result<U144, Vec<u8>> {
+        Ok(sqrt_Q96X48(y))
     }
 
-    pub fn calculate_radius(reserve: U144) -> U144 {
-        calculate_radius(reserve)
+    pub fn calculate_radius(&self, reserve: U144) -> Result<U144, Vec<u8>> {
+        Ok(calculate_radius(reserve))
     }
 
-    pub fn calculateK(depeg_limit: U144, radius: U144) -> U144 {
-        calculateK(depeg_limit, radius)
+    pub fn calculateK(&self, depeg_limit: U144, radius: U144) -> Result<U144, Vec<u8>> {
+        Ok(calculateK(depeg_limit, radius))
     }
 
-    pub fn getTickParameters(depeg_limit: U144, reserve: U144) -> (U144, U144) {
-        getTickParameters(depeg_limit, reserve)
+    pub fn getTickParameters(&self, depeg_limit: U144, reserve: U144) -> Result<(U144, U144), Vec<u8>> {
+        Ok(getTickParameters(depeg_limit, reserve))
     }
 
-    pub fn calculateBoundaryTickS(radius: U144, k: U144) -> U144 {
-        calculateBoundaryTickS(radius, k)
+    pub fn calculateBoundaryTickS(&self, radius: U144, k: U144) -> Result<U144, Vec<u8>> {
+        Ok(calculateBoundaryTickS(radius, k))
     }
 
     pub fn solveQuadraticInvariant(
+        &self,
         delta_linear: U144,
         reserves: Vec<U144>,
         token_in_index: U144,
         token_out_index: U144,
         consolidated_radius: U144,
         k_cross: U144,
-    ) -> U144 {
-        invariant::solveQuadraticInvariant(
+    ) -> Result<U144, Vec<u8>> {
+        Ok(invariant::solveQuadraticInvariant(
             delta_linear,
             reserves,
             token_in_index,
             token_out_index,
             consolidated_radius,
             k_cross,
-        )
+        ))
     }
 
     pub fn calculate_invariant_simple(
+        &self,
         a: i128,
         b: U144,
         r_int: U144,
-    ) -> i128 {
-        invariant::calculate_invariant_simple(a, b, r_int)
+    ) -> Result<i128, Vec<u8>> {
+        Ok(invariant::calculate_invariant_simple(a, b, r_int))
     }
 
-    pub fn signed_div_Q96X48(a: i128, b: i128) -> i128 {
-        div_Q96X48_signed(a, b)
+    pub fn signed_div_Q96X48(&self, a: i128, b: i128) -> Result<i128, Vec<u8>> {
+        Ok(div_Q96X48_signed(a, b))
     }
     
     pub fn calculate_invariant_derivative(
+        &self,
         a: i128,
         b: U144,
         d: U144,
         n: u32,
         x_j: U144,
         sum_reserves: U144,
-    ) -> i128 {
-        invariant::invariant_derivative(a, b, d, n, x_j, sum_reserves)
+    ) -> Result<i128, Vec<u8>> {
+        Ok(invariant::invariant_derivative(a, b, d, n, x_j, sum_reserves))
     }
     
     pub fn calculate_A_B_D(
+        &self,
         sum_reserves: U144,
         sum_reserves_squared: U144,
         n: u32,
@@ -119,11 +118,12 @@ impl OrbitalHelper {
         k_bound: U144,
         r_int: U144,
         s_bound: U144,
-    ) -> (i128, U144, U144) {
-        invariant::calculate_A_B_D(sum_reserves, sum_reserves_squared, n, x_j, k_bound, r_int, s_bound)
+    ) -> Result<(i128, U144, U144), Vec<u8>> {
+        Ok(invariant::calculate_A_B_D(sum_reserves, sum_reserves_squared, n, x_j, k_bound, r_int, s_bound))
     }
 
     pub fn solve_amount_out(
+        &self,
         sum_reserves: U144,
         sum_reserves_squared: U144,
         n: u32,
@@ -131,7 +131,7 @@ impl OrbitalHelper {
         r_int: U144,
         s_bound: U144,
         initial_x_j: U144,
-    ) -> U144 {
-        invariant::solve_amount_out(sum_reserves, sum_reserves_squared, n, k_bound, r_int, s_bound, initial_x_j)
+    ) -> Result<U144, Vec<u8>> {
+        Ok(invariant::solve_amount_out(sum_reserves, sum_reserves_squared, n, k_bound, r_int, s_bound, initial_x_j))
     }
 }
--- contracts/stylus/src/orbital_helper/mod.rs
+++ contracts/stylus/src/orbital_helper/mod.rs
@@ -1,3 +1,4 @@
 pub mod fixed_point;
 pub mod parameters;
 pub mod invariant;
+pub mod errors;
